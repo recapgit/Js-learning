d6d4c753a458ba5d3c2061af39e16fd98b3b0c3f
Rafael <rafaelcassiano185@gmail.com>
2023-03-02 00:59:13 -0300

    estruturas de repetição

--- testeinput.js
+++ testeinput.js
@@ -3,8 +3,19 @@ import inptdados from 'readline-sync';
 //let string = inptdados.question('Insira string: ');
 //console.log('String: ' + string);
 
-let x = Number(inptdados.question("insira um valor x: "));
+/* let x = Number(inptdados.question("insira um valor x: "));
 let y = Number(inptdados.question("insira y: "));
 
 let soma = x+y;
-console.log(soma);
+console.log(soma); */
+
+let carro = { nome: "Fusca", ano: 1969, cor: "azul" }; 
+
+for (let prop in carro) {
+    if (prop == "ano") {    
+            console.log(prop + ": " + carro[prop]);
+            let ano = prop;
+            console.log(ano);
+            console.log(prop[ano]);
+        }
+}
